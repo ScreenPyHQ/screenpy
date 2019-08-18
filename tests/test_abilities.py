a400099a1eb117a48c7576a533f5f3e8fab2103e
Perry Goy <perry@Perrys-MacBook-Pro.local>
2019-08-18 18:21:48 -0500

    add tests for abilities.

---
+++ tests/test_abilities.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+
+from screenpy.abilities.browse_the_web import BrowseTheWeb
+
+
+class TestBrowseTheWeb(TestCase):
+    def test_can_be_instantiated(self):
+        """BrowseTheWeb can be instantiated"""
+        b = BrowseTheWeb.using(None)
+
+        self.assertIsNot(b, None)
