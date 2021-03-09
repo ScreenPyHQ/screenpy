b802e4c83779e12c67172b26b63d0e1bf9aa46ba
bandophahita <trenchrats@gmail.com>
2021-03-09 08:24:45 -0600

    beat logging indentation  (#24)
    * beat logging will indent for each level an action is called that utilizes it.
    
    * enforce formatting standards (and some opinions).
    
    * add proper indentation for asides as well.
    
    Co-authored-by: Perry Goy <perry@Perrys-MacBook-Pro.local>

--- screenpy/pacing.py
+++ screenpy/pacing.py
@@ -7,9 +7,10 @@ and also set the logging severity for Python's built-in logging library.
 
 import logging
 import re
+from contextlib import contextmanager
 from enum import Enum
 from functools import wraps
-from typing import Any, Callable
+from typing import Any, Callable, Generator
 
 import allure
 
@@ -22,6 +23,43 @@ BLOCKER = allure.severity_level.BLOCKER
 
 Function = Callable[..., Any]
 logger = logging.getLogger("screenpy")
+
+
+class IndentManager:
+    """Handle the indentation for CLI logging."""
+
+    def __init__(self) -> None:
+        self.level = 0
+        self.indent = 4
+        self.whitespace = self.indent * " "
+        self.enabled = True
+
+    def add_level(self) -> None:
+        """Increase the indentation level."""
+        self.level += 1
+
+    def remove_level(self) -> None:
+        """Decrease the indentation level."""
+        if self.level > 0:
+            self.level -= 1
+
+    @contextmanager
+    def next_level(self) -> Generator:
+        """Move to the next level of indentation, with context."""
+        self.add_level()
+        try:
+            yield
+        finally:
+            self.remove_level()
+
+    def __str__(self) -> str:
+        if self.enabled:
+            return f"{self.level * self.whitespace}"
+        return ""
+
+
+# Indentation will be managed globally for the run.
+indent = IndentManager()
 
 
 def act(title: str, gravitas: Enum = NORMAL) -> Callable[[Function], Function]:
@@ -97,13 +135,14 @@ def beat(line: str) -> Callable[[Function], Function]:
 
             markers = re.findall(r"\{([^0-9\}]+)}", line)
             cues = {mark: getattr(args[0], mark) for mark in markers}
-            completed_line = line.format(actor, **cues)
-
+            completed_line = f"{indent}{line.format(actor, **cues)}"
             logger.info(completed_line)
             with allure.step(completed_line):
-                retval = func(*args, **kwargs)
-                if retval is not None:
-                    aside(str(retval))
+                with indent.next_level():
+                    retval = func(*args, **kwargs)
+                    if retval is not None:
+                        aside(f"=> {retval}")
+
             return retval
 
         return wrapper
@@ -113,7 +152,8 @@ def beat(line: str) -> Callable[[Function], Function]:
 
 def aside(line: str) -> None:
     """A line spoken in a stage whisper to the audience (log a message)."""
-    logger.info(line)
-    with allure.step(line):
+    completed_line = f"{indent}{line}"
+    logger.info(completed_line)
+    with allure.step(completed_line):
         # Can't call method directly, have to enter or decorate
         pass
