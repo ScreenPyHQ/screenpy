3d97acfb0fafeeeef7ab3a1689ab9112783a6ec2
Perry Goy <perry@Perrys-MacBook-Pro.local>
2021-04-13 21:52:49 -0500

    #50: change direction typing to return Any.

--- screenpy/directions.py
+++ screenpy/directions.py
@@ -2,10 +2,12 @@
 Directions are special functions that ask for something from the Director.
 """
 
+from typing import Any
+
 from .director import Director
 
 
-def noted_under(key: str) -> object:
+def noted_under(key: str) -> Any:
     """Gets a noted value from the director.
 
     Examples::
