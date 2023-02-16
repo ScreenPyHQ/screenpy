3a257e6e8dfcbba9eb11f43fde2d5118c152343c
Marcel Wilson <trenchrats@gmail.com>
2023-02-16 08:34:02 -0600

    Making the test code slighly easier to maintain if new aliases are added in the future.

--- tests/test_actor.py
+++ tests/test_actor.py
@@ -5,6 +5,7 @@ import pytest
 from screenpy import Actor, and_, given, given_that, then, when
 from screenpy.exceptions import UnableToPerform
 from useful_mocks import get_mock_ability_class, get_mock_action_class
+from typing import Any
 
 FakeAction = get_mock_action_class()
 FakeAbility = get_mock_ability_class()
@@ -31,17 +32,23 @@ def test_calls_perform_as() -> None:
 
     action.perform_as.assert_called_once_with(actor)
 
-    actor.did(action)
-    actor.does(action)
-    actor.will(action)
-    actor.should(action)
-    actor.shall(action)
-    actor.was_able_to(action)
-    actor.tries_to(action)
-    actor.tried_to(action)
-    actor.tries(action)
-    actor.tried(action)
-    assert action.perform_as.call_count == 11
+    perform_aliases: list[Any] = [
+        actor.did,
+        actor.does,
+        actor.will,
+        actor.should,
+        actor.shall,
+        actor.was_able_to,
+        actor.tries_to,
+        actor.tried_to,
+        actor.tries,
+        actor.tried,
+    ]
+
+    for alias in perform_aliases:
+        alias(action)
+
+    assert action.perform_as.call_count == len(perform_aliases) + 1
 
 
 def test_complains_for_missing_abilities() -> None:
