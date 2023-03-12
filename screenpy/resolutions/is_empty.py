ac972b089a819246706b2eba29e4f16e6b1f6dca
Perry Goy <perry.goy@gmail.com>
2023-03-11 23:58:33 -0600

    make IsEmpty Resolvable and Describable.

--- screenpy/resolutions/is_empty.py
+++ screenpy/resolutions/is_empty.py
@@ -2,13 +2,17 @@
 Matches an empty collection.
 """
 
+from typing import Sized, TypeVar
+
 from hamcrest import empty
-from hamcrest.library.collection.is_empty import IsEmpty as _IsEmpty
+from hamcrest.core.matcher import Matcher
 
-from .base_resolution import BaseResolution
+from screenpy.pacing import beat
+
+SelfIsEmpty = TypeVar("SelfIsEmpty", bound="IsEmpty")
 
 
-class IsEmpty(BaseResolution):
+class IsEmpty:
     """Match on an empty collection.
 
     Examples::
@@ -16,9 +20,11 @@ class IsEmpty(BaseResolution):
         the_actor.should(See.the(List.of_all(VIDEO_FRAMES), IsEmpty()))
     """
 
-    matcher: _IsEmpty
-    line = "an empty collection"
-    matcher_function = empty
+    def describe(self: SelfIsEmpty) -> str:
+        """Describe the Resolution's expectation."""
+        return "An empty collection."
 
-    def __init__(self) -> None:
-        super().__init__()
+    @beat("... hoping it's an empty collection.")
+    def resolve(self: SelfIsEmpty) -> Matcher[Sized]:
+        """Produce the Matcher to make the assertion."""
+        return empty()
