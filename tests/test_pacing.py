aa668edea3af797b35d68c442df4f6091b684555
Marcel Wilson <trenchrats@gmail.com>
2023-09-25 14:26:13 -0500

    handling corner cases

--- tests/test_pacing.py
+++ tests/test_pacing.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Optional
 
 from screenpy import Actor, IsEqualTo, See, act, aside, beat, scene
 
@@ -22,11 +23,48 @@ class Prop:
 
 class NonesyQuestion:
     @beat("{} examines NonesyQuestion")
-    def answered_by(self, actor: Actor) -> None:
+    def answered_by(self, actor: Actor) -> object:
         return None
 
     def describe(self) -> str:
         return "NonesyQuestion"
+
+
+class CornerCase:
+    @beat("{} examines CornerCase")
+    def answered_by(self, the_actor: Actor) -> object:
+        self.do_not_log_me_man()
+        self.does_return_something()
+        self.no_annotations_rt_none(False)
+        self.no_annotations_rt_int(True)
+        return None
+
+    @beat("Blah!")
+    def do_not_log_me_man(self) -> None:
+        return None
+
+    @beat("Foobar...")
+    def does_return_something(self, toggle: bool = False) -> Optional[int]:
+        if toggle:
+            return 1
+        return None
+
+    # purposfully not annotated
+    @beat("Baz?")
+    def no_annotations_rt_none(self, toggle=False):
+        if toggle:
+            return 1
+        return None
+
+    # purposfully not annotated
+    @beat("Bazinga!!")
+    def no_annotations_rt_int(self, toggle=False):
+        if toggle:
+            return 1
+        return None
+
+    def describe(self) -> str:
+        return "CornerCase"
 
 
 class TestAct:
@@ -73,6 +111,24 @@ class TestBeat:
         assert [r.msg for r in caplog.records] == [
             "Tester sees if nonesyQuestion is equal to None.",
             "    Tester examines NonesyQuestion",
+            "        => None",
+            "    ... hoping it's equal to None.",
+            "        => <None>",
+        ]
+
+    def test_beat_logging_none_corner(self, Tester, caplog):
+        caplog.set_level(logging.INFO)
+        See(CornerCase(), IsEqualTo(None)).perform_as(Tester)
+
+        assert [r.msg for r in caplog.records] == [
+            "Tester sees if cornerCase is equal to None.",
+            "    Tester examines CornerCase",
+            "        Blah!",
+            "        Foobar...",
+            "            => None",
+            "        Baz?",
+            "        Bazinga!!",
+            "            => 1",
             "        => None",
             "    ... hoping it's equal to None.",
             "        => <None>",
