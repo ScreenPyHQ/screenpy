465f023925c9807192231f8f4cb74639670093f4
Marcel Wilson <trenchrats@gmail.com>
2024-02-14 17:59:10 -0600

    adding copyright year test

--- tests/test__version__.py
+++ tests/test__version__.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from datetime import datetime
+
 from screenpy import __version__
 
 
@@ -7,3 +9,8 @@ def test_metadata() -> None:
     assert __version__.__title__ == "screenpy"
     assert __version__.__license__ == "MIT"
     assert __version__.__author__ == "Perry Goy"
+
+
+def test_copyright_year() -> None:
+    current = datetime.now().year
+    assert f"{current}" in __version__.__copyright__
