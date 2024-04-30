37e2bc8ad48f5938de59c52aba0bdcfe77d294c4
Marcel Wilson <trenchrats@gmail.com>
2024-04-30 17:10:25 -0400

    allowing `Matches` to accept `re.compile` (#135)
    * allowing Matches to accept `re.compile`
    
    * updating name of property based on comments in PR
    
    * adding example using `re.compile`
    
    * updated test to illustrate raw string better

--- screenpy/resolutions/matches.py
+++ screenpy/resolutions/matches.py
@@ -1,9 +1,16 @@
 """Matches a string using a regex pattern."""
 
+from __future__ import annotations
+
+from re import Pattern
+from typing import TYPE_CHECKING
+
 from hamcrest import matches_regexp
-from hamcrest.core.matcher import Matcher
 
 from screenpy.pacing import beat
+
+if TYPE_CHECKING:  # pragma: no cover
+    from hamcrest.core.matcher import Matcher
 
 
 class Matches:
@@ -15,16 +22,28 @@ class Matches:
             # matches "/product/1", "/product/22", "/product/942"...
             See.the(Text.of_the(URL), Matches(r"/product/[0-9]{1,3}"))
         )
+
+        # matches "/people/123/edit", "/people/2ec4efef-dec5-9d81bbcc410a/edit"
+        pattern = re.compile(r"/people/.+/edit")
+        the_actor.should(See.the(Text.of_the(URL), Matches(pattern))
+
     """
+
+    @property
+    def pattern_to_log(self) -> str:
+        """Represent the item in a log-friendly way."""
+        if isinstance(self.pattern, Pattern):
+            return f"r'{self.pattern.pattern}'"
+        return f"r'{self.pattern}'"
 
     def describe(self) -> str:
         """Describe the Resolution's expectation."""
-        return f'Text matching the pattern r"{self.pattern}".'
+        return f"Text matching the pattern {self.pattern_to_log}."
 
-    @beat('... hoping it\'s text matching the pattern r"{pattern}".')
+    @beat("... hoping it's text matching the pattern {pattern_to_log}.")
     def resolve(self) -> Matcher[str]:
         """Produce the Matcher to make the assertion."""
         return matches_regexp(self.pattern)
 
-    def __init__(self, pattern: str) -> None:
+    def __init__(self, pattern: str | Pattern[str]) -> None:
         self.pattern = pattern
