adb148df0019bdd87298fdd1f9977aad4afe0fb3
perrygoy <perry.goy@florenceos.com>
2026-04-02 17:52:28 -0500

    REVERT BEFORE MERGE: show example generated test.

---
+++ example_test/conftest.py
@@ -0,0 +1,15 @@
+"""Fixtures for the example ScreenPy test suite."""
+
+from collections.abc import Generator
+
+import pytest
+
+from screenpy import AnActor
+
+
+@pytest.fixture()
+def Perry() -> Generator:
+    """Provide an Actor named Perry for each test."""
+    the_actor = AnActor.named("Perry")
+    yield the_actor
+    the_actor.exit()
